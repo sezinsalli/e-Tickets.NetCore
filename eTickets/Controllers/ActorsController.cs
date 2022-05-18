f9e97d0a25833888efb2c3fdb885fdc5c463d7a8
sezin inyayla <95285144+sezins@users.noreply.github.com>
2022-05-18 10:50:00 +0300

    +

--- eTickets/Controllers/ActorsController.cs
+++ eTickets/Controllers/ActorsController.cs
@@ -21,7 +21,7 @@ namespace eTickets.Controllers
                
         public async Task<IActionResult> Index()
         {
-            var data =await _service.GetAll();
+            var data =await _service.GetAllAsync();
             return View(data);
         }
         //Get: Actors/Create
@@ -29,5 +29,61 @@ namespace eTickets.Controllers
         {
             return View();
         }
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind("FullName,ProfilePictureURL,Bio")] Actor actor)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(actor);
+            }
+            await _service.AddAsync(actor);
+            return RedirectToAction(nameof(Index));
+        }
+        //GET:Actors/Details/1
+        public async Task<IActionResult> Details(int id)
+        {
+            var actorDetails = await _service.GetByIdAsync(id);
+
+            if (actorDetails == null) return View("NotFound");
+            return View(actorDetails);
+        }
+
+        //Get: Actors/Edit/1
+        public async Task<IActionResult> Edit(int id)
+        {
+            var actorDetails = await _service.GetByIdAsync(id);
+            if (actorDetails == null) return View("NotFound");
+            return View(actorDetails);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,FullName,ProfilePictureURL,Bio")] Actor actor)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(actor);
+            }
+            await _service.UpdateAsync(id, actor);
+            return RedirectToAction(nameof(Index));
+        }
+        //Get: Actors/Edit/1
+        public async Task<IActionResult> Delete(int id)
+        {
+            var actorDetails = await _service.GetByIdAsync(id);
+            if (actorDetails == null) return View("NotFound");
+            return View(actorDetails);
+        }
+
+        [HttpPost,ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var actorDetails = await _service.GetByIdAsync(id);
+            if (actorDetails == null) return View("NotFound");
+
+            await _service.DeleteAsync(id);
+            
+            return RedirectToAction(nameof(Index));
+        }
+
     }
 }
--- eTickets/Models/Actor.cs
+++ eTickets/Models/Actor.cs
@@ -12,11 +12,21 @@ namespace eTickets.Models
         public int ActorId { get; set;}
 
         [Display(Name= "Profile Picture URL")]
+        [Required(ErrorMessage = "Profile Picture is required")]
         public string ProfilePictureURL { get; set; }
+
+
         [Display(Name = "Full Name")]
+        [Required(ErrorMessage = "Full Name is required")]
+        [StringLength(50,MinimumLength =3,ErrorMessage ="Full Name mut be between 3 and 50 chars")]
         public string FullName { get; set; }
+
+
         [Display(Name = "Biography")]
+        [Required(ErrorMessage = "Biography is required")]
         public string Bio { get; set; }
+
+
         public List<Actor_Movie> Actor_Movies { get; set; }
     }
 }
